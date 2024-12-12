8b535aadf0e49c04ac7f392adef7fc27db745aae
Nouhaila2023 <nelh237@g.educaand.es>
2024-12-12 21:03:25 +0100

    la practica de examen

--- Programacion/Tema3/Evaluacion/Ejercicio3.java
+++ Programacion/Tema3/Evaluacion/Ejercicio3.java
@@ -3,6 +3,7 @@ package Tema3.Evaluacion;
 import java.util.Arrays;
 import java.util.Scanner;
 
+
 public class Ejercicio3 {
 
     public static void main(String[] args) {
@@ -12,49 +13,37 @@ public class Ejercicio3 {
         //a. Primera letra al final: Mueve la primera letra
         // de cada palabra al final de la palabra
 
-        System.out.println("Dime la frace para el parte A:");
-        String fraceA = sc.nextLine();
-        String palabraA[] = fraceA.split(" ");
+        System.out.println("Dime tu frace:");
+        String frace = sc.nextLine();
+        String palabra[] = frace.split(" ");
 
-        for (int i = 0; i < palabraA.length; i++) {
-            palabraA[i] = palabraA[i].substring(i) + palabraA[i].charAt(0);
-            System.out.println(palabraA[i]);
+        for (int i = 0; i < palabra.length; i++) {
+            palabra[i] = palabra[i].substring(i) + palabra[i].charAt(0);
+            System.out.println(palabra[i]);
         }
 
+        System.out.println("---------------------------------------------");
 
         //b. Reemplazo de vocales: Reemplaza las vocales de la palabra con caracteres
         //especiales: a -> @, e -> &, i -> $, o -> º, u -> #
 
-        String fraceb[] = new String[1];
-        System.out.println("Dime la frace para el parte B");
-        for (int i = 0; i < fraceb.length; i++) {
-            fraceb[i] = sc.nextLine().toLowerCase();
-            for (int j = 0; j < fraceb[i].length(); j++) {
-                char caracteres = fraceb[i].charAt(j);
-                if (caracteres == 'a'){
-                    System.out.print("@");
-                } else if (caracteres == 'e') {
-                    System.out.print("&");
-                } else if (caracteres == 'i') {
-                    System.out.print("$");
-                } else if (caracteres == 'o') {
-                    System.out.print("º");
-                } else if (caracteres == 'u') {
-                    System.out.print("#");
-                } else {
-                    System.out.print(caracteres);
-                }
-            }
+
+        StringBuffer sb = new StringBuffer(frace);
+        for (int i = 0; i < frace.length(); i++) {
+            frace = frace.replace('a' , '@');
+            frace = frace.replace('e' , '&');
+            frace = frace.replace('i' , '$');
+            frace = frace.replace('o' , 'º');
+            frace = frace.replace('u' , '#');
         }
+        System.out.println(frace);
 
 
         //c. Invertir cada palabra: Si la longitud de la palabra es impar, invierte la palabra
         //resultante antes de añadirla al mensaje codificado.
 
-        System.out.println("\nDima la frace para el parte C");
-        String fracec = sc.nextLine();
+        System.out.println("\n---------------------------------------------");
 
-        String palabra[] = fracec.split(" ");
 
         for (int i = 0; i < palabra.length; i++) {
             if (palabra[i].length() % 2 != 0){
